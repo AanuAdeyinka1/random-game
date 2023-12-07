29a5c9c53e60b69a523ebee3244bf141a7738307
Aanu lawal <lawalaanu14@gmail.com>
2023-12-07 09:41:25 +0100

    changes

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ let randomNumber = Math.floor(Math.random() * 100) + 1;
 console.log(randomNumber);
 
 function guessNumber() {
-  const randomGuess = prompt(`guess the number between 1 to 100`);
+  const randomGuess = console.log(`guess the number between 1 to 100`);
 
   if (randomGuess == randomNumber) {
-    prompt(`Guess is correct`);
+    console.log(`Guess is correct`);
   } else if (randomGuess < randomNumber) {
-    alert(`Guess too low` + `  try again`);
+    console.log(`Guess too low` + `  try again`);
   } else {
-    alert(`Guess too high!`);
+    console.log(`Guess too high!`);
   }
 }
 guessNumber();
